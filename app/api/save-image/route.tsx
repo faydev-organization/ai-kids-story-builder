9e85a2f25399f35bc9c53913fae0263091839dcf
Fayash Wahyudha <wahyudhafayash@Fayashs-MacBook-Air.local>
2024-10-01 00:27:54 +0700

    change title name

--- app/api/save-image/route.tsx
+++ app/api/save-image/route.tsx
@@ -14,7 +14,19 @@ export async function POST(req: NextRequest) {
     }
 
     // Convert the image
-    const base64Image = "data:image/png;base64," + (await convertImage(url));
+    const convertImage = async (imageUrl: string) => {
+      try {
+        const response = await axios.get(imageUrl, {
+          responseType: "arraybuffer",
+        });
+        const base64Image = Buffer.from(response.data).toString("base64");
+        return base64Image;
+      } catch (error) {
+        throw new Error("Failed to convert image"); // Rethrow error to be caught in the main handler
+      }
+    };
+
+    const base64Image = await convertImage(url);
     if (!base64Image) {
       return NextResponse.json(
         { message: "Failed to convert image" },
@@ -27,26 +39,18 @@ export async function POST(req: NextRequest) {
 
     // Upload the image
     await uploadString(imageRef, base64Image, "data_url");
+    console.log("File Uploaded");
 
     // Get download URL
     const downloaderUrl = await getDownloadURL(imageRef);
+    // console.log(downloaderUrl);
 
     return NextResponse.json({ imageUrl: downloaderUrl });
   } catch (error: any) {
+    // console.error("Error in save-image handler:", error);
     return NextResponse.json(
       { message: "Internal Server Error", error: error.message },
       { status: 500 }
     );
   }
 }
-
-// Helper function to convert image
-const convertImage = async (imageUrl: string) => {
-  try {
-    const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
-    const base64Image = Buffer.from(response.data).toString("base64");
-    return base64Image;
-  } catch (error) {
-    throw new Error("Failed to convert image");
-  }
-};
