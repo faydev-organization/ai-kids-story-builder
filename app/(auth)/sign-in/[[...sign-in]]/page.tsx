df24640f32631c773c630593e5110a57f1f588f0
Fayash Wahyudha <wahyudhafayash@Fayashs-MacBook-Air.local>
2024-10-01 00:38:06 +0700

    fix bg auth

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="grid grid-cols1 md:grid-cols-2 h-full">
-      <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
+      <div className="flex justify-center items-center">
         <Image
-          src={"/login.png"}
+          src="/login.png"
           alt="login"
-          width={700}
-          height={1000}
-          className="w-full"
+          width={700} // Original width
+          height={1000} // Original height
+          className="max-w-full h-auto" // Responsive image
         />
       </div>
-      <div className="flex justify-center items-center h-screen order-first md:order-last">
+      <div className="flex justify-center items-center h-full order-first md:order-last">
         <SignIn />
       </div>
     </div>
